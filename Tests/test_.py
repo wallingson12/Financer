076a4d1587e64ab76e800524940b733b82f8524b
wallingson12 <wallingson12@yahoo.com.br>
2026-03-14 16:01:58 -0300

    Fix: Corrigir estrutura de investimentos e adicionar múltiplos investimentos por usuário

--- Tests/test_.py
+++ Tests/test_.py
@@ -13,15 +13,15 @@ from backend.models.main import Conta
 # -------------------------------------------------
 def criar_dados_exemplo():
     return pd.DataFrame({
-        'Data': pd.to_datetime([
+        'data': pd.to_datetime([
             '2024-01-10',
             '2024-01-15',
             '2024-02-05'
         ]),
-        'Tipo': ['Pix', 'Boleto', 'Cartão'],
-        'Detalhe': ['Cliente A', 'Internet', 'Supermercado'],
-        'Crédito (R$)': [1000, 500, 200],
-        'Débito (R$)': [200, 100, 50]
+        'tipo': ['Pix', 'Boleto', 'Cartão'],
+        'detalhe': ['Cliente A', 'Internet', 'Supermercado'],
+        'credito': [1000, 500, 200],
+        'debito': [200, 100, 50]
     })
 
 
